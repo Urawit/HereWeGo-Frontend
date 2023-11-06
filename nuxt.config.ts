b4de2441571f06442e595ced56bd7adecd4c2278
Atitaya <priow111555@gmail.com>
2023-11-06 17:42:49 +0700

    activity detail

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css',
+  '@fortawesome/fontawesome-free/css/all.css',
+  '@fortawesome/fontawesome-svg-core/styles.css',
+],
   modules: [
     '@pinia/nuxt',
     '@pinia-plugin-persistedstate/nuxt',
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
     <h1>activity list</h1>
+    <div class="m-4 text-blue-600">
+      <nuxt-link :to="'/activity/' + 2">
+        detail
+      </nuxt-link>
+    </div>
 </template>
 
 <script>
