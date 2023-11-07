d7667421c0a032a55bbebcd0f1538824895d4929
Atitaya <priow111555@gmail.com>
2023-11-07 08:32:01 +0700

    profile

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,67 +1,24 @@
 <template>
-<<<<<<< HEAD
-  <div class="relative h-80 w-full bg-gradient-to-t from-gray-400 via-gray-300 to-gray-200">
-    <div class="absolute -bottom-20 ml-60 h-40 w-40 -translate-x-1/2 transform rounded-full border-4 border-white overflow-hidden">
-        <img :src="`http://localhost/${image_path}`" alt="User's Profile Picture" class="w-full h-full object-cover rounded-full" />
-    </div>
-  </div>
-  <div class="h-80 w-full bg-gray-50">
-    <h1 class="ml-96 text-3xl font-semibold"> {{ username }}</h1>
-    <!-- <p class="mt-3 my-2 ml-96 text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing</p> -->
-    <div v-for="activity in activities" :key="activity.id" class="mb-4 ml-96 gap-2 mt-8 w-auto flex flex-wrap">
-      <span class="cursor-pointer rounded-full bg-gray-300 px-4 py-2 text-sm">{{ activity.name }}</span>
-    </div>
-  </div>
-</template>
 
-<script>
-import { useAuthStore } from '~/stores/useAuthStore';
-import { computed, ref, onMounted} from "vue";
-import useMyFetch from '~/composables/useMyFetch';
-    export default{
-        setup(){
-            const activities = ref([]);  // to store the fetched activities
-            const route = useRoute();
-
-            async function fetchActivities() {
-            const { data } = await useMyFetch<any>(
-                `selectActivities?user_id=${route.params.id}`,
-                {}
-            );
-            activities.value = data;
-            }
-
-            onMounted(fetchActivities);
-            const authStore = useAuthStore();
-            const image_path = computed(() => authStore.user.image_path);
-            const username = computed(() => authStore.user.username);
-            return {
-                image_path,
-                username,
-                activities
-            }
-            }
-        };
-=======
     <div class=" px-6 py-32 relative text-center text-neutral-200 bg-blue-800">
     </div>
     <div class="bg-white relative shadow rounded-lg w-5/6 mx-auto">
         <div class="flex justify-left">
-            <img v-if="!image_path" src="`http://localhost/${user.image_path}`" class="rounded-full mx-auto absolute -top-16 w-40 h-40 shadow-md border-4 border-white transition duration-200 transform hover:scale-110"/>
-            <img v-else :src="`http://localhost/${user.image_path}`" class="rounded-full mx-auto absolute -top-16 w-40 h-40 shadow-md border-4 border-white transition duration-200 transform hover:scale-110" />
+            <!-- <img v-if="!image_path" src="`http://localhost/${user.image_path}`" class="rounded-full mx-auto absolute -top-16 w-40 h-40 shadow-md border-4 border-white transition duration-200 transform hover:scale-110"/>
+            <img v-else :src="`http://localhost/${user.image_path}`" class="rounded-full mx-auto absolute -top-16 w-40 h-40 shadow-md border-4 border-white transition duration-200 transform hover:scale-110" /> -->
         </div>
     </div>
     <div class="container mb-4">
         <div id="content" class="bg-white w-screen h-screen ">
             <div class="mx-80 text-2xl pt-3 text-black">
                 <div id="username text-2xl text-black">
-                    Username, 18
+                    auth, 18
                 </div>
                 <div id="name" class="my-2 text-gray-400">
                     Firstname Lastname
                 </div>
                 <div class="py-10 bg-white">
-                    
+
                 </div>
                 <div class="flex w-full">
                     <div class="group grid h-16 flex-grow card rounded-box place-items-center bg-white">
@@ -122,6 +79,14 @@ import useMyFetch from '~/composables/useMyFetch';
     </div>
 </template>
 
-<script>
->>>>>>> 18e93a9f440bf14cdfbed907d7af41970d9870ee
+<script lang = "ts">
+import useMyFetch from '~/composables/useMyFetch';
+
+// const authStore = useAuthStore();
+// const user_id = computed(() => authStore.user.id);
+// const user_name = computed(() => authStore.user.name);
+// const user_image_path = computed(() => authStore.user.image_path);
+
+const { data: activities} = await useMyFetch<any>("myActivities", {})
+console.log(activities)
 </script>
