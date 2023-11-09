411cea56c173d095ca50c38b74771aca0c328258
Atitaya <priow111555@gmail.com>
2023-11-10 03:46:15 +0700

    profile

--- pages/user/[id].vue
+++ pages/user/[id].vue
@@ -1,45 +1,83 @@
-
 <template>
-    <div class="relative h-80 w-full bg-gradient-to-t from-gray-400 via-gray-300 to-gray-200">
-      <div class="absolute -bottom-20 ml-60 h-40 w-40 -translate-x-1/2 transform rounded-full border-4 border-white overflow-hidden">
-          <img :src="`http://localhost/${image_path}`" alt="User's Profile Picture" class="w-full h-full object-cover rounded-full" />
+  <div class=" px-6 py-32 relative text-center text-neutral-200 bg-blue-800">
+  </div>
+  <div class="bg-white relative shadow rounded-lg w-5/6 mx-auto">
+    <div class="flex justify-left">
+      <img :src="`http://localhost/${user.image_path}`"
+        class="rounded-full mx-auto absolute -top-16 w-40 h-40 shadow-md border-4 border-white transition duration-200 transform hover:scale-110" />
+    </div>
+  </div>
+  <div class="container mb-4">
+    <div id="content" class="bg-white w-screen ">
+      <div class="mx-80 text-2xl pt-3 text-black">
+        <div id="username text-2xl text-black">
+          {{ user.username }}
+        </div>
+        <!-- <div class="mb-4 mt-10 w-auto flex flex-wrap">
+          <span class="cursor-pointer rounded-full bg-gray-300 px-4 py-2 text-sm mr-4 "></span>
+        </div> -->
+        <div class="py-10 bg-white">
+        </div>
+
+     
+
+        <!-- <div class="mb-4 mt-10 w-auto flex flex-wrap">
+          <span v-for="(activity, index) in activities" :key="index"
+            class="cursor-pointer rounded-full bg-gray-300 px-4 py-2 text-sm mr-4">
+            {{ activity.name }}
+          </span>
+        </div> -->
       </div>
     </div>
-    <div class="h-80 w-full bg-gray-50">
-      <h1 class="ml-96 text-3xl font-semibold"> {{ username }}</h1>
-      <!-- <p class="mt-3 my-2 ml-96 text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing</p> -->
-      <div v-for="activity in activities" :key="activity.id" class="mb-4 ml-96 gap-2 mt-8 w-auto flex flex-wrap">
-        <span class="cursor-pointer rounded-full bg-gray-300 px-4 py-2 text-sm">{{ activity.name }}</span>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  import { useAuthStore } from '~/stores/useAuthStore';
-  import { computed, ref, onMounted} from "vue";
-  import useMyFetch from '~/composables/useMyFetch';
-      export default{
-          setup(){
-              const activities = ref([]);  // to store the fetched activities
-              const route = useRoute();
-  
-              async function fetchActivities() {
-              const { data } = await useMyFetch<any>(
-                  `selectActivities?user_id=${route.params.id}`,
-                  {}
-              );
-              activities.value = data;
-              }
-  
-              onMounted(fetchActivities);
-              const authStore = useAuthStore();
-              const image_path = computed(() => authStore.user.image_path);
-              const username = computed(() => authStore.user.username);
-              return {
-                  image_path,
-                  username,
-                  activities
-              }
-              }
-          };
-  </script>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useAuthStore } from '~/stores/useAuthStore';
+import { computed, onMounted } from "vue";
+
+const route = useRoute()
+const user = ref({});
+
+onMounted(() => {
+  getUser();
+  checkFriendStatus();
+});
+const getUser = async () => {
+  try {
+    const response = await useMyFetch<any>(`find-user/${route.params.id}`, {});
+    console.log("response: ", response.data.value);
+    user.value = (response.data.value);
+  } catch (error) {
+    console.error('Error fetching user:', error);
+  }
+}
+
+const checkFriendStatus = async () => {
+  try {
+    const friendId =  user.value.id ;
+    const response = await fetch('http://localhost/api/statusFriend', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        friend_id: friendId
+      })
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log('Friend Status:', data.result);
+    } else {
+      console.error('Failed to fetch friend status');
+    }
+  } catch (error) {
+    
+    //console.log("error")
+    console.log('Error checking friend status:', error);
+  }
+};
+
+</script>
